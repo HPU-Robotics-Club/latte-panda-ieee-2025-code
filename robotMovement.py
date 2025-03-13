e421482f1bfc753c291c650dd84efd5577524a3e
Nathanial-Keatts <nkeatts@highpoint.edu>
2025-03-13 17:35:49 -0400

    Add files via upload
    This is the python code for the robot movement

---
+++ robotMovement.py
@@ -0,0 +1,126 @@
+#These are the pins, meaning front/rear driver/passenger
+PinFD1 = Pin(0, Pin.OUT)
+PinFD2 = Pin(0, Pin.OUT)
+PinFP1 = Pin(0, Pin.OUT)
+PinFP2 = Pin(0, Pin.OUT)
+PinRD1 = Pin(0, Pin.OUT)
+PinRD2 = Pin(0, Pin.OUT)
+PinRP1 = Pin(0, Pin.OUT)
+PinRP2 = Pin(0, Pin.OUT)
+
+#These are all of the direction constants
+D_FORWARD        = 0
+D_BACKWARD       = 1
+D_LEFT           = 2
+D_RIGHT          = 3
+D_FORWARD_LEFT   = 4
+D_FORWARD_RIGHT  = 5
+D_BACKWARD_LEFT  = 6
+D_BACKWARD_RIGHT = 7
+D_END            = 8 #Stops the robot's motion
+
+#These are here for some reason to make it easy to change the code later maybe
+MOTOR_FORWARD  =  1
+MOTOR_BACKWARD = -1
+MOTOR_OFF      =  0
+
+#These are the rotation constant
+R_CW             = 0
+R_CCW            = 1
+R_END            = 2 #Stops the robot's motion
+
+
+def stopMotion():
+    driveMotor(FD_1, FD_2, 0)
+    driveMotor(RD_1, RD_2, 0)
+    driveMotor(FP_1, FP_2, 0)
+    driveMotor(RP_1, RP_2, 0)
+
+#Start move8 Defintion
+def move8(int direction):
+    if direction == D_FORWARD: #Forward
+        driveMotor(FD_1, FD_2, MOTOR_FORWARD)
+        driveMotor(FP_1, FP_2, MOTOR_FORWARD)
+        driveMotor(RD_1, RD_2, MOTOR_FORWARD)
+        driveMotor(RP_1, RP_2, MOTOR_FORWARD)
+
+    elif direction == D_BACKWARD:#Backward
+        driveMotor(FD_1, FD_2, MOTOR_BACKWARD)
+        driveMotor(FP_1, FP_2, MOTOR_BACKWARD)
+        driveMotor(RD_1, RD_2, MOTOR_BACKWARD)
+        driveMotor(RP_1, RP_2, MOTOR_BACKWARD)
+
+    elif direction == D_LEFT:#Left
+        driveMotor(FD_1, FD_2, MOTOR_FORWARD)
+        driveMotor(FP_1, FP_2, MOTOR_BACKWARD)
+        driveMotor(RD_1, RD_2, MOTOR_BACKWARD)
+        driveMotor(RP_1, RP_2, MOTOR_FORWARD)
+
+    elif direction == D_RIGHT:#Right
+        driveMotor(FD_1, FD_2, MOTOR_BACKWARD)
+        driveMotor(FP_1, FP_2, MOTOR_FORWARD)
+        driveMotor(RD_1, RD_2, MOTOR_FORWARD)
+        driveMotor(RP_1, RP_2, MOTOR_BACKWARD)
+
+    elif direction == D_FORWARD_RIGHT:#Forward Right
+        driveMotor(FD_1, FD_2, MOTOR_FORWARD)
+        driveMotor(FP_1, FP_2, MOTOR_OFF)
+        driveMotor(RD_1, RD_2, MOTOR_OFF)
+        driveMotor(RP_1, RP_2, MOTOR_FORWARD)
+
+    elif direction == D_FORWARD_LEFT:#Forward Left
+        driveMotor(FD_1, FD_2, MOTOR_OFF)
+        driveMotor(FP_1, FP_2, MOTOR_FORWARD)
+        driveMotor(RD_1, RD_2, MOTOR_FORWARD)
+        driveMotor(RP_1, RP_2, MOTOR_OFF)
+
+    #NOTE I am not sure these two are correct
+    elif direction == D_BACKWARD_LEFT:#Backward Left
+        driveMotor(FD_1, FD_2, MOTOR_BACKWARD)
+        driveMotor(FP_1, FP_2, MOTOR_OFF)
+        driveMotor(RD_1, RD_2, MOTOR_OFF)
+        driveMotor(RP_1, RP_2, MOTOR_BACKWARD)
+
+    elif direction == D_BACKWARD_RIGHT: #Backward Right
+        driveMotor(FD_1, FD_2, MOTOR_OFF)
+        driveMotor(FP_1, FP_2, MOTOR_BACKWARD)
+        driveMotor(RD_1, RD_2, MOTOR_BACKWARD)
+        driveMotor(RP_1, RP_2, MOTOR_OFF)
+
+    elif direction == D_END: #Ends Movement
+        driveMotor(FD_1, FD_2, MOTOR_OFF)
+        driveMotor(RD_1, RD_2, MOTOR_OFF)
+        driveMotor(FP_1, FP_2, MOTOR_OFF)
+        driveMotor(RP_1, RP_2, MOTOR_OFF)
+#End move8 Defintion
+
+#Start rotate Definition
+def rotate(int direction):
+    if direction == R_CW:
+        driveMotor(FD_1, FD_2, MOTOR_FORWARD)
+        driveMotor(RD_1, RD_2, MOTOR_FORWARD)
+        driveMotor(FP_1, FP_2, MOTOR_BACKWARD)
+        driveMotor(RP_1, RP_2, MOTOR_BACKWARD)
+    elif direction ==  R_CCW:
+        driveMotor(FD_1, FD_2, MOTOR_BACKWARD)
+        driveMotor(RD_1, RD_2, MOTOR_BACKWARD)
+        driveMotor(FP_1, FP_2, MOTOR_FORWARD)
+        driveMotor(RP_1, RP_2, MOTOR_FORWARD)
+    elif direction R_END:
+        driveMotor(FD_1, FD_2, MOTOR_OFF);
+        driveMotor(RD_1, RD_2, MOTOR_OFF);
+        driveMotor(FP_1, FP_2, MOTOR_OFF);
+        driveMotor(RP_1, RP_2, MOTOR_OFF);
+#End rotate Definition
+
+#maybe will be added later
+def move360(double direction):
+    return
+
+def driveMotor(Pin pin1, Pin pin2, int power):
+    if power < 0:
+        pin1.value(power)
+        pin2.value(MOTOR_OFF)
+    else:
+        pin1.value(MOTOR_OFF)
+        pin2.value(-power)
