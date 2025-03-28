65511ba32d0a6c3f7504a2fe019d7744c26b207f
Walmqrt <46291857+Zenqrt@users.noreply.github.com>
2025-03-28 12:35:37 -0400

    surely

--- robot.py
+++ robot.py
@@ -1,10 +1,13 @@
+import time
 from enum import Enum
 from pyfirmata import Arduino, util, SERVO
 import cv2
 
 import robot_movement as mv
-import states
 from util.april_tag import AprilTagDetector
+
+
+LIGHT_SENSOR_PIN = 4
 
 
 class RobotState(Enum):
@@ -22,22 +25,45 @@ camera = cv2.VideoCapture()
 state = RobotState.WAITING
 
 
+def is_light_on() -> bool:
 
+    # When a bright light is detected (at least that's what this is supposed to be)
+    if board.analog[LIGHT_SENSOR_PIN].read() < 1000:
+        return True
+
+    return False
+
+time_start = 0
 
 def update():
+    global state
+
     match state:
         case RobotState.WAITING:
-            states.waiting.run()
+            if is_light_on():
+                state = RobotState.ESCAPE_START
         case RobotState.ESCAPE_START:
-            states.escape_start.run()
+            global time_start
+            now = time.time()
+
+            if time_start == 0:
+                time_start = now
+            elif now - time_start >= 1:
+                mv.move8(mv.D_FORWARD)
+            else:
+                state = RobotState.MOVE_BOX
         case RobotState.MOVE_BOX:
-            states.move_box.run()
+            # states.move_box.run()
+            pass
         case RobotState.GATHER_OUTSIDE:
-            states.gather_outside.run()
+            # states.gather_outside.run()
+            pass
 
 # Setup
 board = Arduino('')
 board.digital[mv.ARM_SERVO].mode = SERVO
+it = util.Iterator(board)
+it.start()
 
 while True:
     update()
